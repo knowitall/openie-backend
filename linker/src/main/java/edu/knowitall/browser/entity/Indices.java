34d9731b0bb8e4eb8c82082c08ad1ce6d9b664c4
Justin Huang <jstn@cs.washington.edu>
2013-05-13 18:10:03 -0700

    Rewrite Indices to be a Derby connection.

--- linker/src/main/java/edu/knowitall/browser/entity/Indices.java
+++ linker/src/main/java/edu/knowitall/browser/entity/Indices.java
@@ -1,78 +1,37 @@
 package edu.knowitall.browser.entity;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.regex.Pattern;
 
-public class Indices {
-    private static final Pattern tabSplit = Pattern.compile("\t");
-    private static Map<String, Integer> fbidIndexMap = null;
+import edu.knowitall.browser.util.DerbyDb;
 
-    public Indices(String basePath) {
-        fbidIndexMap = loadFbidIndices(basePath + "browser-freebase/indices.txt");
-    }
-
-    private synchronized Map<String, Integer> loadFbidIndices(String resourceName) {
-        System.err.println("Loading fbid indices");
-
-        try {
-        BufferedReader reader = new BufferedReader(
-            new InputStreamReader(new FileInputStream(resourceName))
-        );
-
-        Map<String, Integer> nakedMap = new TreeMap<String, Integer>();
-
-        String line;
-        while ((line = reader.readLine()) != null) {
-
-            String[] split = tabSplit.split(line);
-            if (split.length < 2) {
-                System.err.println("Bad line: " + line);
-                continue;
-            } else {
-
-                try {
-                    int num = Integer.parseInt(split[1]);
-                    nakedMap.put(split[0], num);
-                } catch (NumberFormatException nfe) {
-                    nfe.printStackTrace();
-                }
-            }
-
-        }
-        System.err.println("Done loading fbid indices");
-
-        return Collections.unmodifiableMap(nakedMap);
-        } catch (IOException e) {
-          throw new RuntimeException(e);
-        }
-    }
-
-    public synchronized Map<String, Integer> fbidToIndexMap() throws IOException {
-        return fbidIndexMap;
-    }
-
-    /**
-     * Convert and ToString()
-     * 
-     * @throws IOException
-     */
-    public List<Integer> convertFbids(List<String> fbids) throws IOException {
-        List<Integer> indices = new ArrayList<Integer>(fbids.size());
-        for (String fbid : fbids) {
-            if (fbid != null && fbidToIndexMap().containsKey(fbid)) {
-                indices.add(fbidToIndexMap().get(fbid));
-            }
-        }
-        return Collections.unmodifiableList(indices);
-    }
-
+public class Indices extends DerbyDb {
+  public Indices(String url) {
+      super(url);
+  }
+  
+  public List<Integer> getIndices(List<String> fbids) {
+      List<Integer> indices = new ArrayList<Integer>();
+      for (String fbid : fbids) {
+          PreparedStatement statement = null;
+          try {
+              statement = connection.prepareStatement(
+                  "SELECT index FROM fbid_indices WHERE fbid=?"
+              );
+              statement.setString(1, fbid);
+              ResultSet results = statement.executeQuery();
+              while (results.next()) {
+                  int index = results.getInt(1);
+                  indices.add(index);
+              }
+              statement.close();
+          } catch (SQLException e) {
+              throw new RuntimeException("Exception with FBID indices lookup.", e);
+          }
+      }
+      return indices;
+  }
 }
