3bb6dbcb86c343cd3ec5ebc5fa219f1feb68e13b
Rob Bart <rbart@cs.washington.edu>
2013-10-17 13:23:08 -0700

    Fix basedir bug.

--- linker/src/main/scala/edu/knowitall/browser/entity/Constants.scala
+++ linker/src/main/scala/edu/knowitall/browser/entity/Constants.scala
@@ -5,7 +5,7 @@ import java.io.File
 object Constants {
   // Parent directory with lots of supporting data.
   def mainIndexPath(basePath: File) = new File(basePath, "browser-freebase")
-  
+
   // Supporting data for entity linking.
   def fbidToTitleInlinksFile(basePath: File) = {
     new File(mainIndexPath(basePath), "fbid_to_line.sorted")
@@ -16,12 +16,12 @@ object Constants {
   def cachedMapFile(basePath: File) = {
     new File(mainIndexPath(basePath), "cached.sorted")
   }
-  
+
   // Supporting data for entity linking context matching.
   def batchMatchPath(basePath: File) = {
     new File(new File(mainIndexPath(basePath), "3-context-sim"), "index")
   }
-  
+
   // Supporting data for entity typing.
   def typeLookupIndexPath(basePath: File) = {
     new File(mainIndexPath(basePath), "type-lookup-index")
@@ -29,11 +29,11 @@ object Constants {
   def freeBaseTypeEnumFile(basePath: File) = {
     new File(mainIndexPath(basePath), "fbTypeEnum.txt")
   }
-  
+
   // Derby database paths.
   val defaultDerbyDbBasePath = new File("/scratch2")
   def entityLinkingDbPath(basePath: File) = {
-    new File("/scratch2", "entitylinking")
+    new File(basePath, "entitylinking")
   }
   // TODO: add crosswikis table to entitylinking DB.
   def crosswikisDbPath(basePath: File) = {
